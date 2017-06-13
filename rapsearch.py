401b9e958a20dd7e56d396aa09e4b47c09a6462b
Dillon Barker <dor.barker@gmail.com>
2017-06-13 15:37:07 -0600

    table_generator(); de-linting

--- rapsearch.py
+++ rapsearch.py
@@ -1,24 +1,27 @@
+'''Manages running RAPSearch and processing RAPSearch output for SURPI'''
+
 import subprocess
 import re
 from Bio import SeqIO
 from utilities import concatenate
+from taxonomy_lookup import taxonomy_lookup
 
 def run_rapsearch(query, output, database, cores, cutoff, fast, log):
     '''Runs RAPSearch with some default arguments overriden.'''
 
-    a = 'T' if fast else 'F'
+    fast_mode = 'T' if fast else 'F'
 
     rapsearch_cmd = ('rapsearch', '-q', query, '-o', output, '-d', database,
                      '-z', str(cores), '-e', cutoff, '-v', '1', '-b', '1',
-                     '-t', 'N', '-a', a)
+                     '-t', 'N', '-a', fast_mode)
 
     rapsearch_stdout = subprocess.check_output(rapsearch_cmd,
                                                stderr=subprocess.STDOUT,
                                                universal_newlines=True)
 
     try:
-        with open(log, 'w') as f:
-            f.write(rapsearch_stdout)
+        with open(log, 'w') as logfile:
+            logfile.write(rapsearch_stdout)
 
     except TypeError:
         pass
@@ -28,11 +31,11 @@ def remove_octothorpe_lines(filepath):
     writes back into the same file.
     '''
 
-    with open(filepath, 'r') as i:
-        lines = [l for l in i.readlines() if l.startswith('#')]
+    with open(filepath, 'r') as infile:
+        lines = [l for l in infile.readlines() if l.startswith('#')]
 
-    with open(filepath, 'w') as o:
-        o.writelines(lines)
+    with open(filepath, 'w') as outfile:
+        outfile.writelines(lines)
 
 def subseq(parent, query, output):
     '''Runs external process seqtk subseq.
@@ -44,15 +47,15 @@ def subseq(parent, query, output):
 
     result = subprocess.check_output(subseq_cmd, universal_newlines=True)
 
-    with open(output, 'w') as f:
-        f.write(result)
+    with open(output, 'w') as outfile:
+        outfile.write(result)
 
 def fasta_to_seq(fastafile):
     '''Reads a fasta file, and returns a list of headerless sequences'''
 
-    with open(fastafile, 'r') as f:
+    with open(fastafile, 'r') as fasta:
 
-        recs = SeqIO.parse(f, 'fasta')
+        recs = SeqIO.parse(fasta, 'fasta')
 
         seqs = [str(rec.seq) for rec in recs]
 
@@ -63,39 +66,61 @@ def paste(a_list, b_list):
     tab separated and newline terminated
     '''
 
-    def merge(a, b):
-        return '{}\t{}\n'.format(a.strip(), b.strip())
+    def merge(a_elem, b_elem):
+        return '{}\t{}\n'.format(a_elem.strip(), b_elem.strip())
 
-    return [merge(a, b) for a, b in zip(a_list, b_list)]
+    return [merge(a_elem, b_elem) for a_elem, b_elem in zip(a_list, b_list)]
 
 def filter_taxonomy(pattern, annotated):
     '''Filters the taxonomy annotation file based on a regular expression.
 
     Returns a list of matching patterns.
     '''
-    p = re.compile(pattern)
+    comppat = re.compile(pattern)
 
-    with open(annotated, 'r') as f:
+    with open(annotated, 'r') as annot:
 
-        return [line for line in f if re.search(p, line)]
+        return [line for line in annot if re.search(comppat, line)]
 
-def rapsearch(rapsearch_query, rapsearch_output, database, cores,
-              cutoff, fast, log):
+def table_generator(annotated, snap_rap, acc, species, genus, family):
+    '''Runs external script `table_generator.sh`'''
+
+    assert snap_rap in ('SNAP', 'RAP')
+    assert acc in ('Y', 'N')
+    assert species in ('Y', 'N')
+    assert genus in ('Y', 'N')
+    assert family in ('Y', 'N')
+
+    cmd = ('table_generator.sh', str(annotated),
+           snap_rap, acc, species, genus, family)
+
+    subprocess.check_call(cmd)
+
+def rapsearch_shared(rapsearch_query, rapsearch_output, rap_database,
+                     tax_db_dir, cores, cutoff, fast, log):
     '''Functions shared by all RAPSearch strategies.'''
 
-    m8 = rapsearch_output.with_suffix( rapsearch_output.suffix  + '.m8')
-    m8fasta = m8.with_suffix( m8.suffix + '.fasta' )
+    annotated = rapsearch_output.with_suffix('.annotated')
+    addseq = rapsearch(rapsearch_query, rapsearch_output, rap_database,
+                       cores, cutoff, fast, log)
+
+    taxonomy_lookup(infile=addseq, outfile=annotated, db_dir=tax_db_dir,
+                    seq_type='nucl', file_type='blast')
+
+def rapsearch(query, output, database, cores, cutoff, fast, log):
+
+    m8 = output.with_suffix(output.suffix + '.m8')
+    m8fasta = m8.with_suffix(m8.suffix + '.fasta')
     addseq = m8.with_suffix('.addseq.m8')
 
-    run_rapsearch(rapsearch_query, rapsearch_output,
-                  database, cores, cutoff, fast, log)
 
+    run_rapsearch(query, output, database, cores, cutoff, fast, log)
 
     remove_octothorpe_lines(m8)
 
     m8_lines = m8.read_text().splitlines()
 
-    subseq(unmatched, m8, m8fasta)
+    subseq(query, m8, m8fasta)
 
     seqs = fasta_to_seq(m8fasta)
 
@@ -103,38 +128,48 @@ def rapsearch(rapsearch_query, rapsearch_output, database, cores,
 
     addseq.write_text('\n'.join(pasted))
 
-    # TODO: taxonomy lookup goes here
+    return addseq
 
 def rapsearch_viral(query, vir_output, nr_output, vir_database, nr_database,
                     cores, vir_cutoff, nr_cutoff, fast, abyss_output,
-                    contigs_nt_unmatched_fasta):
+                    contigs_nt_unmatched_fasta, tax_db_dir):
 
     # TODO: make contigs_nt_unmatched generated internally rather than
     # passing in by parameter
 
-    m8 = output.with_suffix( output.suffix  + '.m8')
-    log = output.with_suffix('.virlog')
+    m8 = vir_output.with_suffix(vir_output.suffix  + '.m8')
+    log = vir_output.with_suffix('.virlog')
+    virus_tax = nr_output.with_suffix('.viruses.annotated')
+    contig_tax = nr_output.with_suffix('.NRcontigs.annotated')
 
-    rapsearch(query, vir_output, vir_database, cores, cutoff, fast, log)
+    rapsearch_shared(query, vir_output, vir_database, tax_db_dir,
+                     cores, vir_cutoff, fast, log)
 
-    # TODO: table_generator goes here
+    table_generator(vir_output.with_suffix('.annotated'),
+                    'RAP', 'N', 'Y', 'N', 'N')
 
-    concatenate(m8, abyss_output, contigs_nt_unmatched_fasta)
+    concatenate(m8, abyss_output, output=contigs_nt_unmatched_fasta)
+
     assert contigs_nt_unmatched_fasta.exists(), "Contigs.NTunmatched.fasta \
                                                  doesn't exist"
 
-    rapsearch(contigs_nt_unmatched_fasta, nr_output, nr_database, cores,
-              nr_cutoff, fast, log.with_suffix('.nrlog'))
+    rapsearch_shared(contigs_nt_unmatched_fasta, nr_output, nr_database,
+                     tax_db_dir, cores, nr_cutoff, fast,
+                     log.with_suffix('.nrlog'))
 
     viruses = filter_taxonomy('Viruses', nr_output)
-    contigs = filter_taxonomy('^contig', nr_output)
+    virus_tax.write_text('\n'.join(viruses))
 
-    # TODO: table_generator goes here
+    table_generator(virus_tax, 'RAP', 'Y', 'Y', 'Y', 'Y')
+
+    contigs = filter_taxonomy('^contig', nr_output)
+    contig_tax.write_text('\n'.join(contigs))
+
+    table_generator(contig_tax, 'RAP', 'Y', 'Y', 'Y', 'Y')
+
 
     # TODO: coverage mapping
 
     # TODO: Get viral geaders no longer found in NR rapsearch
 
     # TODO: table_generator goes here
-
-
