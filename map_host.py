b949b9fafbac43572c49b05d1b45fe3efc78f731
Dillon Barker <dor.barker@gmail.com>
2017-06-23 20:17:52 -0600

    Type annotations to map_host.py

--- map_host.py
+++ map_host.py
@@ -5,18 +5,22 @@ import subprocess
 from pathlib import Path
 from utilities import user_msg, logtime, annotated_to_fastq
 
-def vmtouch(snap_db):
+def vmtouch(snap_db: Path) -> bool:
     '''Calls vmtouch to check if the SNAP database is loaded into memory'''
 
-    cmd = ('vmtouch', '-m500G', snap_db)
+    cmd = ('vmtouch', '-m500G', str(snap_db))
     output = subprocess.check_output(cmd, universal_newlines=True).splitlines()
 
     for line in output:
-        if 'Resident Pages' in line:
+        if 'Resident Pages' in line and line.split()[4] == '100%':
+            in_memory = True
+            break
+    else:
+        in_memory = False
 
-            return line.split()[4] == '100%'
+    return in_memory
 
-def set_snap_cache_option(snap_db):
+def set_snap_cache_option(snap_db: Path) -> str:
     '''Sets SNAP options based on whether the database is loaded into memory'''
 
     if vmtouch(snap_db):
@@ -33,13 +37,15 @@ def set_snap_cache_option(snap_db):
 
     return snap_cache_option
 
-def host_subtract(preprocessed, snap_db_dir, edit_distance, temp_dir, cores):
+def host_subtract(preprocessed: Path, snap_db_dir: Path, edit_distance: int,
+                  temp_dir: Path, cores: int) -> Path:
     '''Subtracts preprocessed reads from host SNAP databases.
 
     Writes the subtracted fastq and returns its file path.
     '''
     @logtime('Host subtraction')
-    def subtract(snap_db, to_subtract, output, pre):
+    def subtract(snap_db: Path, to_subtract: Path,
+                 output: Path, pre: str) -> None:
         '''Prepare and execute the SNAP search'''
 
         subtract_cmd = ('snap-aligner', 'single', snap_db, to_subtract,
@@ -48,20 +54,20 @@ def host_subtract(preprocessed, snap_db_dir, edit_distance, temp_dir, cores):
                         '-d', edit_distance, '-n', '25', '-F', 'u',
                         '-map', pre)
 
-        subprocess.call(map(str, subtract_cmd))
+        subprocess.call(list(map(str, subtract_cmd)))
         # end subtract()
 
     subtracted_fastq = preprocessed.with_suffix('.fastq')
 
     to_subtract = preprocessed
 
-    with tempfile.TemporaryDirectory(dir=temp_dir) as temp_dir_:
+    with tempfile.TemporaryDirectory(dir=str(temp_dir)) as ephemeral:
 
         for i, snap_db in enumerate(snap_db_dir.glob('*')):
 
-            snap_cache_option = set_snap_cache_option(snap_db)
+            temp_output = Path(ephemeral) / str(i)
 
-            temp_output = Path(temp_dir_) / i
+            snap_cache_option = set_snap_cache_option(snap_db)
 
             subtract(snap_db, to_subtract, temp_output, snap_cache_option)
 
